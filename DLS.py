aa796a512377c400cc0f30c767fbc368e0db2768
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2020-07-20 18:46:25 +0300

    more projects

--- DLS.py
+++ DLS.py
@@ -7,7 +7,7 @@ import random
 moves = ["defend", "shoot", "load"]
 #move = ["defend", "load"]
 
-keep_playing = "true"
+keep_playing = True
 
 
 countBc = 0 # bullets computer
@@ -15,20 +15,7 @@ countBp = 0 # bullets player
 countWin = 0
 countLose = 0
 
-#pmoves = input("you must load, enter load: ")
-#cmoves = random.choice(move)
-
-#if pmoves == "load":
-    #countBp +=1
-    #print("Stack: ", countBp)
-#else:
-    #print("you donwt have bullets you must load")
-
-#if cmoves == "load":
-    #countBc +=1
-    #print("Stack: ", countBc)
-
-while keep_playing == "true":  #and countBp> 0 and countBc > 0:
+while keep_playing:
 
     cmove = random.choice(moves)
     pmoves = input("what is your move: defend, load or shoot ?")
@@ -42,49 +29,48 @@ while keep_playing == "true":  #and countBp> 0 and countBc > 0:
     elif cmove == "load":
         countBc +=1
     print("bullets p: ", countBp, "bullets c: ", countBc)
+
 # if your stack is empty
     if pmoves == "shoot" and countBp == 0:
         print("No bullets in your stack")
     elif cmove == "shoot" and countBc ==0:
         print("No bullets in the stack")
 
+while  keep_playing and countBc > 0 or countBp > 0:
+    # the way to win or lose
 
-# the way to win or lose
-    elif countBp > 0 or countBc > 0:
-
-        if pmoves == "shoot" and cmove == "load":
-            countBp -=1
-            print("bullets p: ", countBp, "bullets c: ", countBc)
-            countWin +=1
-            print("You win!!")
-
-
-        elif pmoves == "load" and cmove == "shoot":
-            countBc -=1
-            print("bullets p: ", countBp, "bullets c: ", countBc)
-            countLose +=1
-            print("You lose ):")
-
-    # defence and protect
-        if pmoves == "defend" and cmove == "shoot":
-            print("good you blocked him")
-            countBc -=1
-            print("bullets p: ", countBp, "bullets c: ", countBc)
+            if pmoves == "shoot" and cmove == "load":
+                countBp -=1
+                print("bullets p: ", countBp, "bullets c: ", countBc)
+                countWin +=1
+                print("You win!!")
 
 
-        elif pmoves == "shoot" and cmove == "defend":
-            print("Damm, lets try again!")
-            countBp -=1
-            print("bullets p: ", countBp, "bullets c: ", countBc)
+            elif pmoves == "load" and cmove == "shoot":
+                countBc -=1
+                print("bullets p: ", countBp, "bullets c: ", countBc)
+                countLose +=1
+                print("You lose ):")
 
-# how to win the whole game
-        #if countWin == 3:
-         #   print("YOU WIN THE GAME!!")
-          #  break
-         #elif countLose == 3:
-          #   print("GAME OVER ):")
-           #  break
+        # defence and protect
+            if pmoves == "defend" and cmove == "shoot":
+                print("good you blocked him")
+                countBc -=1
+                print("bullets p: ", countBp, "bullets c: ", countBc)
 
+
+            elif pmoves == "shoot" and cmove == "defend":
+                print("Damm, lets try again!")
+                countBp -=1
+                print("bullets p: ", countBp, "bullets c: ", countBc)
+
+    # how to win the whole game
+            #if countWin == 3:
+             #   print("YOU WIN THE GAME!!")
+              #  break
+             #elif countLose == 3:
+              #   print("GAME OVER ):")
+               #  break
 
 
 
